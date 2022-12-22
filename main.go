c6f3bcebf1c852d9d92edeb04db59937a97115d2
Manuel Carrillo <manuel@smartprintsink.com>
2022-12-22 15:28:55 -0800

    prettyfie dis bytes

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package prettyfie
 
 import "encoding/json"
 
-func Pretty(data interface{}) string {
+func Pretty(data any) string {
 	pretty, _ := json.MarshalIndent(data, " ", "  ")
+	return string(pretty)
+}
+
+func DisBytes(data []byte) string {
+	var aux any
+
+	json.Unmarshal(data, &aux)
+	pretty, _ := json.MarshalIndent(aux, " ", "  ")
 	return string(pretty)
 }
